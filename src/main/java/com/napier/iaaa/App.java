6ab8dceccad5168a7a1586dc79f428683d32894a
40653006 <40653006@live.napier.ac.uk>
2024-11-14 15:41:12 +0000

    Runs on Docker

--- src/main/java/com/napier/iaaa/App.java
+++ src/main/java/com/napier/iaaa/App.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class App {
     // Database connection details
-    static final String DB_URL = "jdbc:mysql://localhost:3306/world";
+    static final String DB_URL = "jdbc:mysql://db:3306/world";
     static final String USER = "root";
     static final String PASSWORD = "root";
 
